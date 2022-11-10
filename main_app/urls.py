3999924a8919cfcab879fc4277c2d241e2d897f4
Damian <bzdyrad@gmail.com>
2022-11-10 12:21:03 -0500

    create recipe model

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('recipes/', views.recipes_index, name='recipes_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
 # Add the following import
-from django.http import HttpResponse
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+  Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
-  return HttpResponse('HELLO')
+  return render(request, 'about.html')
+
+def recipes_index(request):
+  return render(request, 'recipes/index.html', { 'cats': cats })
